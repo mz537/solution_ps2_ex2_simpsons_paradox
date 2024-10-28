4fb91a42a55afe8a6e5efb3e02dbec0a0f8f87f9
Adrian Ochs <15180657+adrianochs@users.noreply.github.com>
2024-10-28 09:11:34 +0100

    sol ps2 ex2

--- README.md
+++ README.md
@@ -2,4 +2,27 @@
 
 Solutions to problem set 2, ex.2.
 
-The data is from (https://calmcode.io/datasets/smoking)[https://calmcode.io/datasets/smoking].
+The challenge is from (https://calmcode.io/datasets/smoking)[https://calmcode.io/datasets/smoking].
+
+# Setup
+
+To install the required packages, run the following command in the terminal:
+
+```bash
+conda env create -f environment.yml
+conda activate smoke
+```
+
+## Pre-commit
+
+This repository uses pre-commit to run some checks before each commit. To install pre-commit, run:
+
+```bash
+pre-commit install
+```
+
+To run the checks manually, run:
+
+```bash
+pre-commit run --all-files
+```
--- create_some_fake_cols.py
+++ create_some_fake_cols.py
@@ -1,12 +1,20 @@
+# This script is to make the data from calmcode.io on smoking a bit
+# more interesting
+# by adding some fake columns to it and some null values.
+# All are random and not based on any real data. So it should
+# not affect the analysis.
+
 import numpy as np
 import pandas as pd
 
 
 def create_some_fake_cols(df: pd.DataFrame) -> pd.DataFrame:
-    # create random 0 1 column to indicate gener
+    # create random 0 1 column to indicate gender; 50-50 split
     df["gender"] = np.random.choice([0, 1], df.shape[0], p=[0.5, 0.5])
     df["gender"] = df["gender"].map({0: "female", 1: "male"})
     df["salary"] = np.random.randint(1000, 100000, df.shape[0])
+    # random missings in salary column
+    df.loc[np.random.choice(df.index, 1000), "salary"] = np.nan
     df.to_csv("data/smoking_data.csv")
     return df
 
@@ -14,3 +22,4 @@ def create_some_fake_cols(df: pd.DataFrame) -> pd.DataFrame:
 if __name__ == "__main__":
     df = pd.read_csv("data/smoking.csv")
     df = create_some_fake_cols(df)
+    print("Done making the data a bit more difficult to work with.")
